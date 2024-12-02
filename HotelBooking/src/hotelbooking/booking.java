55dea788da9cc80573662f7d66b17d86eaa00f2a
MustafaA61 <mustafaanwaralt2@gmail.com>
2024-12-02 09:40:29 -0700

    Add files via upload

---
+++ HotelBooking/src/hotelbooking/booking.java
@@ -0,0 +1,10 @@
+package hotelbooking;
+
+public class booking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hotelbookingsystem/src/hotelbookingsystem/hotelbooking.java
@@ -0,0 +1,10 @@
+package hotelbookingsystem;
+
+public class hotelbooking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
